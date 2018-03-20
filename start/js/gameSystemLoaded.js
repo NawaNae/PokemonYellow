ba6a1daff5f77826506c04815ac447256030d573
LAPTOP-4938AERP\ltue2 <ltue2129@gmail.com>
2018-03-21 01:11:44 +0800

    新增GameSystemLoaded.js並修改載入順序

---
+++ start/js/gameSystemLoaded.js
@@ -0,0 +1,7 @@
+function gameSystemLoaded()
+{
+    var GS=GameSystem;
+    var CL=GS.Classes;
+    GS.protagonist=new CL.Protagonist("小智障(於GameLoaded)");
+}
+gameSystemLoaded();
--- start/js/loadGame.js
+++ start/js/loadGame.js
@@ -2,12 +2,14 @@
 var loadGameEnd;
 Load.js.import(
 [
-        { src: define.jsPath + 'myMenu.js', lookFor: 'MyMenu' },
         { src: define.jsPath + 'gameSystem.js', lookFor: 'GameSystem' },
         { src: define.jsPath + 'level.js', lookFor: 'GameSystem.Classes.Level' },
         { src: define.jsPath + 'mapWalker.js', lookFor: 'GameSystem.Classes.MapWalker' },
         { src: define.jsPath + 'character.js', lookFor: 'GameSystem.Classes.Character' },
+        { src: define.jsPath + 'protagonist.js', lookFor: 'GameSystem.Classes.Protagonist' },
         { src: define.jsPath + 'text.js', lookFor: 'GameSystem.Classes.Text' },
+        { src: define.jsPath + 'gameSystemLoaded.js', lookFor: 'gameSystemLoaded' },
+        { src: define.jsPath + 'myMenu.js', lookFor: 'MyMenu' },
         { src: define.jsPath + 'myGameLevelTest.js', lookFor: 'MyGameTest' },
         { src: define.jsPath+'mainGame.js' }//請最後載入mainGame
 ]);
