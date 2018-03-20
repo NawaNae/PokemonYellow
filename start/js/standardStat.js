13146c22bad25ac67b4d747c585b127885da97ad
PrismRealm <alan159753@gmail.com>
2018-03-20 18:40:29 +0800

    新增 「寶可夢」與「招式」上的屬性列舉

--- start/js/standardStat.js
+++ start/js/standardStat.js
@@ -35,3 +35,55 @@ class StandardStat {
     set speed(newSpeed) { if (newSpeed >= 0) this._speed = newSpeed; else throw new Error("\"speed\" value must >= 0."); }
     get speed() { return this._speed; }
 }
+
+/**
+ * 列舉。為寶可夢或招式的「屬性」。
+ * @readonly
+ * @enum {Symbol}
+ */
+GameSystem.Classes.StandardStat.Type = Object.freeze({
+    /** 「一般」屬性 */
+    Normal: Symbol("Normal"),
+
+    /** 「格鬥」屬性 */
+    Fighting: Symbol("Fighting"),
+
+    /** 「飛行」屬性 */
+    Flying: Symbol("Flying"),
+
+    /** 「毒」屬性 */
+    Poison: Symbol("Poison"),
+
+    /** 「地面」屬性 */
+    Ground: Symbol("Ground"),
+
+    /** 「岩石」屬性 */
+    Rock: Symbol("Rock"),
+
+    /** 「蟲」屬性 */
+    Bug: Symbol("Bug"),
+
+    /** 「幽靈」屬性 */
+    Ghost: Symbol("Ghost"),
+
+    /** 「火」屬性 */
+    Fire: Symbol("Fire"),
+
+    /** 「水」屬性 */
+    Water: Symbol("Water"),
+
+    /** 「草」屬性 */
+    Grass: Symbol("Grass"),
+
+    /** 「電」屬性 */
+    Electric: Symbol("Electric"),
+
+    /** 「超能力」屬性 */
+    Phycsic: Symbol("Phycsic"),
+
+    /** 「冰」屬性 */
+    Ice: Symbol("Ice"),
+
+    /** 「龍」屬性 */
+    Dragon: Symbol("Dragon")
+});
