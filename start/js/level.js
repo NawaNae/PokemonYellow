619b330614ce2eea4a49acf6fb9f2d702dd211bf
LAPTOP-4938AERP\ltue2 <ltue2129@gmail.com>
2018-03-20 13:33:46 +0800

    GameSystem.Classes.Leve Init

---
+++ start/js/level.js
@@ -0,0 +1,16 @@
+GameSystem.Classes.Level=
+class Level extends Framework.Level
+{
+    constructor(size={x:0,y:0})
+    {
+        this.map;
+        this.size=new GameSystem.Classes.Rectangle({x:0,y:0},size||{x:0,y:0});
+        this.obstacles=new Array();
+        this.eventObj=new Array();
+        this.npcs= new Array();
+        this.gates= new Array();
+        this.battleFields= new Array();
+    }
+
+    
+}
