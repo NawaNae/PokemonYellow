b208e0fe62822793efa0af0058de53de3ca39b60
LAPTOP-4938AERP\ltue2 <ltue2129@gmail.com>
2018-03-20 23:28:38 +0800

    新增index.html用重新導向的方式轉至start.html 將loadGame中移除的項目去除

--- start/js/loadGame.js
+++ start/js/loadGame.js
@@ -8,7 +8,6 @@ Load.js.import(
         { src: define.jsPath + 'mapWalker.js', lookFor: 'GameSystem.Classes.MapWalker' },
         { src: define.jsPath + 'character.js', lookFor: 'GameSystem.Classes.Character' },
         { src: define.jsPath + 'text.js', lookFor: 'GameSystem.Classes.Text' },
-        { src: define.jsPath + 'walkCounter.js', lookFor: 'GameSystem.Classes.OneBlockCount' },
         { src: define.jsPath + 'myGameLevelTest.js', lookFor: 'MyGameTest' },
         { src: define.jsPath+'mainGame.js' }//請最後載入mainGame
 ]);
