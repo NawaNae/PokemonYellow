abff781b32ad4cbb48b5cc2e70aa94406811cbf7
NawaNawa <ltue2129@gmail.com>
2018-03-22 01:24:46 +0800

    角色class新增移動向量陣列

--- start/js/character.js
+++ start/js/character.js
@@ -4,6 +4,13 @@ class Character {
         this._name = name;
         this._position = position || new GameSystem.Classes.Position(0, 0);
         this._image = image;
+        this.movePositionVector=//地圖移動向量陣列
+        {
+            Up:new GameSystem.Classes.Position(0,-1),
+            Down:new GameSystem.Classes.Position(0,+1),
+            Right:new GameSystem.Classes.Position(+1,0),
+            Left:new GameSystem.Classes.Position(-1,0),
+        }
     }
 
     set name(newName) { this._name = newName; }
