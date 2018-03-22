6d2edb3438dec768e766fdc92e2d2c9b38ae7fd3
NawaNawa <ltue2129@gmail.com>
2018-03-22 15:28:08 +0800

    新增MapPosition

---
+++ start/js/mapPosition.js
@@ -0,0 +1,11 @@
+GameSystem.Classes.MapPosition=
+class MapPosition
+{
+    constructor(name,position)
+    {
+        this.mapName=name||"";
+        this.position=position||new GameSystem.Classes.Position();
+        if(!name)console.log('請確保初始化Name或者之後設定mapName否則Connection地圖可能出錯');
+        if(!position)console.log('請確保初始化Position或者之後設定position否則連接點會預設在地圖邊界');
+    }
+}
