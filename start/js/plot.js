d3d5cd10e8771a797e8ee30bf7eb4f4773d6a043
PrismRealm <alan159753@gmail.com>
2018-03-23 14:00:07 +0800

    新增 劇情類別(Plot)

---
+++ start/js/plot.js
@@ -0,0 +1,24 @@
+/**
+ * @class Plot
+ * @classdesc 劇情，在玩家與NPC或物件對話時所會用到的內容安排。
+ * 
+ * @prop {string} name 劇情的名稱。
+ * @prop {Paragraph[]} content 劇情的內容安排。
+ */
+GameSystem.Classes.Plot =
+class Plot {
+    /**
+     * @param {string} name 劇情的名稱。
+     * @param {Paragraph[]?} content 劇情的內容安排。
+     */
+    constructor(name, content = []) {
+        this._name = name;
+        this._content = content;
+    }
+
+    set name(newName) { this._name = newName; }
+    get name() { return this._name; }
+
+    set content(newContent) { this._content = newContent; }
+    get content() { return this._content; }
+}
