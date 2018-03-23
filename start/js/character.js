68222ce84474b5b57c4fa350586680544534621d
PrismRealm <alan159753@gmail.com>
2018-03-23 12:24:40 +0800

    新增 角色類別(Character)的面朝與註解

--- start/js/character.js
+++ start/js/character.js
@@ -1,7 +1,25 @@
+/**
+ * @class Character
+ * @abstract
+ * @classdesc 一個角色必備的資料。
+ * 
+ * @prop {string} name 角色的名稱。
+ * @prop {GameSystem.Classes.Position} position 角色所在地圖上的位置。
+ * @prop {GameSystem.Classes.Image} image 角色的圖片。
+ * @prop {GameSystem.Classes.Position{}} movePositionVector 角色的位移向量。
+ * @prop {GameSystem.Classes.Character.Face} facing 角色的面向。
+ */
 GameSystem.Classes.Character = 
 class Character {
-    constructor(name = "", position, image) {
+    /**
+     * @param {string} name 角色的姓名。
+     * @param {GameSystem.Classes.Character.Face} face 角色的面朝方向
+     * @param {GameSystem.Classes.Position} position 角色的位置
+     * @param {GameSystem.Classes.Image} image 角色的圖片
+     */
+    constructor(name, face, position, image) {
         this._name = name;
+        this._facing = face;
         this._position = position || new GameSystem.Classes.Position(0, 0);
         this._image = image;
         this.movePositionVector=//地圖移動向量陣列
@@ -21,3 +39,22 @@ class Character {
     get image() { return image; }
 
 };
+
+/**
+ * 列舉。表示角色的面朝方向。
+ * @readonly
+ * @enum {number}
+ */
+GameSystem.Classes.Character.Face = Object.freeze({
+    /** 角色的面向為「上」 */
+    Up: Symbol("Up"),
+
+    /** 角色的面向為「右」 */
+    Right: Symbol("Right"),
+
+    /** 角色的面向為「下」 */
+    Down: Symbol("Down"),
+
+    /** 角色的面相為「左」 */
+    Left: Symbol("Right")
+});
