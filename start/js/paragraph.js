b0f98fe720c60cbcde79c558ad1a4474dd2ef88a
PrismRealm <alan159753@gmail.com>
2018-03-23 14:09:22 +0800

    新增 段落類別(Paragraph)

---
+++ start/js/paragraph.js
@@ -0,0 +1,24 @@
+/**
+ * @class Paragraph
+ * @classdesc 段落，表示在劇情中每一段的段落文字以及關連動作。
+ * 
+ * @prop {string} content 段落的內容。
+ * @prop {GameSystem.Classes.Action} 此段落要執行的動作。
+ */
+GameSystem.Classes.Paragraph =
+class Paragraph {
+    /**
+     * @param {string} content 段落的內容。
+     * @param {GameSystem.Classes.Action} action 此段落要執行的動作。
+     */
+    constructor(content, action) {
+        this._content = content;
+        this._action = action;
+    }
+
+    set content(newContent) { this._content = newContent; }
+    get content() { return this._content; }
+
+    set action(newAction) { this._action = newAction; }
+    get action() { return this._action; }
+}
