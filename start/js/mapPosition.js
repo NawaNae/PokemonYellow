28010e98e16b019c79e7438bb0cc99fb5fdd471c
NawaNawa <ltue2129@gmail.com>
2018-03-29 00:05:21 +0800

    新增 一些sublevel與parent屬性於mapPosition

--- start/js/mapPosition.js
+++ start/js/mapPosition.js
@@ -1,12 +1,23 @@
 GameSystem.Classes.MapPosition=
 class MapPosition
 {
-    constructor(name,position)
+    constructor(name,position,config={parentName:undefined,isParentLevel:undefined})
     {
         this.mapName=name||"";
         this.isSubLevel=false;
+        this.isParentLevel=config.isParentLevel;
+        this._parentName="";
+        this.parentName=config.parentName||"";
         this.position=position||new GameSystem.Classes.Position();
         if(!name)console.log('請確保初始化Name或者之後設定mapName否則Connection地圖可能出錯');
         if(!position)console.log('請確保初始化Position或者之後設定position否則連接點會預設在地圖邊界');
+    }
+    set parentName(value){this.isSubLevel=true;this._parentName=value;}
+    get parentName(){return this._parentName;}
+    isSubLevelOf(parentName)
+    {
+        if(this.isSubLevel)
+            return this._parentName==parentName;
+        return false;
     }
 }
