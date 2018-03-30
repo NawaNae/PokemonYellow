9489d1d6d8a764d4fa971c92eec8574657082588
NawaNawa <ltue2129@gmail.com>
2018-03-30 22:04:57 +0800

    新增 npc平順移動

--- start/js/mapWalker.js
+++ start/js/mapWalker.js
@@ -1,8 +1,11 @@
 GameSystem.Classes.MapWalker=
 class MapWalker
 {
-    constructor(config={mapRef,lockTime,walkSpeed:1,blockWidth:16,timesPerPixel})
+    constructor(config={mapRef,npcsRef,lockTime,walkSpeed:1,blockWidth:16,timesPerPixel})
     {
+        let GS=GameSystem;
+        let CS=GS.Classes;
+        let PT=CS.Point;
         this._blockWidth=config.blockWidth||16;//格子寬
         this._timesPerPixel=config.timesPerPixel||1;//每格子播放幾次
         this.lockTime=config.lockTime||GameSystem.Manager.Key.lockTime||300;//鎖鍵時間
@@ -16,21 +19,22 @@ class MapWalker
             Down:0,
             Right:0
         };
+        this.npcs=config.npcsRef||Framework.Game._currentLevel.npcs;//npcs之ref
         this.map=config.mapRef||Framework.Game._currentLevel.map;//map之reference
         this.map.game=this.map.game||{};//new namespce
         this.map.game.newPoint=new GameSystem.Classes.Point();//目標點(播放結束直接設定為該點 以防移動不準確)
         this.speed=config.walkSpeed||1;//移動速度(格數/每周期)
-        this.moveVector=//地圖移動向量陣列
+        this.mapMoveVector=//地圖移動向量陣列
         {
-            Up:new GameSystem.Classes.Point(0,-1),
-            Down:new GameSystem.Classes.Point(0,+1),
-            Right:new GameSystem.Classes.Point(+1,0),
-            Left:new GameSystem.Classes.Point(-1,0),
+            Up:new GameSystem.Classes.Point(0,+1),
+            Down:new GameSystem.Classes.Point(0,-1),
+            Right:new GameSystem.Classes.Point(-1,0),
+            Left:new GameSystem.Classes.Point(+1,0),
         }
+        this.characterMoveVector=CS.Character.MovePointVector;//地圖移動向量陣列
+        
         this.timeout;
-        let GS=GameSystem;
-        let CS=GS.Classes;
-        let PT=CS.Point;
+        
         this.keyInput=(e)=>//keyInput的事件
         {
             let key = GS.Manager.Key.moveKeys[e.key];
@@ -45,22 +49,32 @@ class MapWalker
                         var protagonistNewPoint=GS.protagonist.position.toPoint();
                         var protagonistScreenPoint=GS.protagonist._screenPosition.toPoint();
       
-                        this.map.game.newPoint=new PT(protagonistScreenPoint.x-protagonistNewPoint.x,
-                            protagonistScreenPoint.y-protagonistNewPoint.y);
+                        this.map.game.newPoint=new PT(
+                            protagonistScreenPoint.x-protagonistNewPoint.x,
+                            protagonistScreenPoint.y-protagonistNewPoint.y
+                        );
                     }
                     let move=this.moveIncrease(key,this.speed);
                     this.map.x+=move.x;
                     this.map.y+=move.y;
-                    
+                    for(let npc of this.npcs)
+                    {
+                        npc.image.position.x+=move.x;
+                        npc.image.position.y+=move.y;
+                    }
                     this.walkCounter[key]+=this.speed;//increase
                     setTimeout(this.timeout,this.movePeriod);//continue to next loop...
                 }
                 else
                 {    
                     this.map.position=this.map.game.newPoint;
-                   
+                    for(let npc of this.npcs)
+                    {
+                        //console.log(npc);
+                        npc.updateImagePosition();
+                    }
                     this.walkCounter[key]=0;//reset(initialize)
-                    console.log("walk anime end");
+                   // console.log("walk anime end");
                 }
             };
             this.timeout();
@@ -78,12 +92,14 @@ class MapWalker
         this._blockWidth=value;
         this._countEnd=this._timesPerPixel*this._blockWidth;
     }
-    moveIncrease(key,value)
+    moveIncrease(key,value,moveVector)
     {
         value=value||1;
         key=GameSystem.Manager.Key.keyMapping[key];
-        var aimVector=this.moveVector[key];
-        return new GameSystem.Classes.Point(-aimVector.x*value/this._timesPerPixel,-aimVector.y*value/this._timesPerPixel);
+        moveVector=moveVector||this.mapMoveVector;
+        var aimVector=moveVector[key];
+        return new GameSystem.Classes.Point(aimVector.x*value/this._timesPerPixel,aimVector.y*value/this._timesPerPixel);
 
     }
+    
 }
