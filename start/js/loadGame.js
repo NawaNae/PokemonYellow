7e79cb14471cb93fc469d9f0cd2e872bc83b538a
PrismRealm <alan159753@gmail.com>
2018-04-14 14:05:56 +0800

    測試 加入部分戰鬥有關的資源檔

--- start/js/loadGame.js
+++ start/js/loadGame.js
@@ -13,6 +13,7 @@ Load.js.import(
         { src: define.jsPath + 'option.js', lookFor: 'GameSystem.Classes.Option' },
         { src: define.jsPath + 'yesNoDialog.js', lookFor: 'GameSystem.Classes.YesNoDialog' },
         { src: define.jsPath + 'HTMLObjectContainer.js', lookFor: 'GameSystem.Classes.HTMLObjectContainer' },
+        { src: define.jsPath + 'battlePad.js', lookFor: 'GameSystem.Classes.BattlePad' },
         { src: define.jsPath + 'level.js', lookFor: 'GameSystem.Classes.Level' },
         { src: define.jsPath + 'mapWalker.js', lookFor: 'GameSystem.Classes.MapWalker' },
         { src: define.jsPath + 'character.js', lookFor: 'GameSystem.Classes.Character' },
@@ -20,16 +21,23 @@ Load.js.import(
         { src: define.jsPath + 'plot.js', lookFor: 'GameSystem.Classes.Plot' },
         { src: define.jsPath + 'npc.js', lookFor: 'GameSystem.Classes.NPC' },
 
+        { src: define.jsPath + 'standardStat.js', lookFor: 'GameSystem.Classes.StandardStat' },
+        { src: define.jsPath + 'pokemon.js', lookFor: 'GameSystem.Classes.Pokemon' },
+
         { src: define.jsPath + 'protagonist.js', lookFor: 'GameSystem.Classes.Protagonist' },
         { src: define.jsPath + 'text.js', lookFor: 'GameSystem.Classes.Text' },
         { src: define.jsPath + 'mapPosition.js', lookFor: 'GameSystem.Classes.MapPosition' },
         { src: define.jsPath + 'connection.js', lookFor: 'GameSystem.Classes.Connection' },
 
+        /** 測試用，若有問題可以暫時註解 */
+        { src: define.jsPath + 'battleAnimation.js', lookFor: 'GameSystem.Classes.BattleAnimation' },
+
         { src: define.jsPath + 'gameSystemLoaded.js', lookFor: 'gameSystemLoaded' },
         { src: define.jsPath + 'myMenu.js', lookFor: 'MyMenu' },
         { src: define.jsPath + 'levelPalletTown.js', lookFor: 'PalletTown' },
         { src: define.jsPath + 'levelRoute01.js', lookFor: 'Route01' },
-        { src: define.jsPath+'mainGame.js' }//請最後載入mainGame
+        { src: define.jsPath + 'battleLevel.js', lookFor: 'BattleLevel' },
+        { src: define.jsPath+'mainGame.js' } //請最後載入mainGame
 ]);
 
 
