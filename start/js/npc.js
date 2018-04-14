b0d0c7c4153756425492f24c2221034a67da5899
PrismRealm <alan159753@gmail.com>
2018-04-14 14:01:08 +0800

    新增 將屬性pokemons追加至NPC中

--- start/js/npc.js
+++ start/js/npc.js
@@ -5,6 +5,7 @@
  * 
  * @prop {GameSystem.Classes.Position[]?} behavior 此NPC的移動動作。
  * @prop {GameSystem.Classes.Plot?} plot 觸發此NPC時所會產生的劇情。
+ * @prop {GameSystem.Classes.Pokemon[]?} pokemons 此NPC所擁有的寶可夢。
  */
 GameSystem.Classes.NPC =
 class NPC extends GameSystem.Classes.Character {
@@ -15,12 +16,14 @@ class NPC extends GameSystem.Classes.Character {
      * @param {GameSystem.Classes.Character.Face} face 此NPC的面向。
      * @param {GameSystem.Classes.Behavior?} behavior 此NPC的動作。(可選項)
      * @param {GameSystem.Classes.Plot?} plot 觸發此NPC時所會產生的對話情景或動作。(可選項)
+     * @param {GameSystem.Classes.Pokemon?} pokemons 此NPC所擁有的寶可夢。(可選項)
      */
-    constructor(name, face, position, image, behavior, plot) {
+    constructor(name, face, position, image, behavior, plot, pokemons) {
         super(name, face, position, image);
 
         this._behavior = behavior;
         this._plot = plot;
+        this._pokemons = pokemons || [];
     }
 
     set behavior(newBehavior) { this._behavior = newBehavior; }
@@ -28,4 +31,7 @@ class NPC extends GameSystem.Classes.Character {
 
     set plot(newPlot) { this._plot = newPlot; }
     get plot() { return this._plot; }
+
+    set pokemons(newPokemons) { this._pokemons = newPokemons; }
+    get pokemons() { return this._pokemons; }
 }
