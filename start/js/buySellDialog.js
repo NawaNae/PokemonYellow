797e278c4637363954e8306964071be3cca555c8
NawaNawa <ltue2129@gmail.com>
2018-04-20 00:33:48 +0800

    新增 買賣dialog

---
+++ start/js/buySellDialog.js
@@ -0,0 +1,30 @@
+GameSystem.Classes.BuySellDialog=
+class BuySellDialog extends GameSystem.Classes.Options
+{
+    constructor()
+    {
+        super({className:"buySellDialog"});
+        let GS=GameSystem,CS=GS.Classes;
+        let Option=GS.Classes.Option;
+        this.autoChangeInputMode=true;
+        this.visible=false;
+        this.inputMode=GameSystem.Classes.Level.InputModes.buySellDialog;
+        this.push(new Option("買",()=>{this.buySelectSend()}))
+        this.options[0].select=true;
+        this.push(new Option("賣",()=>{this.sellSelectSend()}))
+        this.push(new Option("取消",()=>{this.cancelSelectSend()}))
+
+    }
+    buySelectSend()
+    {
+        GameSystem.HTMLObjectContainer.shoppingList.show();
+    }
+    sellSelectSend()
+    {
+        GameSystem.HTMLObjectContainer.shoppingList.show();
+    }
+    cancelSelectSend()
+    {
+        this.visible=false;
+    }
+}
