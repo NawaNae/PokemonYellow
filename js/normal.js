11f730d60545747e71cfca0d0940e6b2463600b4
NawaNawa <ltue2129@gmail.com>
2018-04-21 19:10:37 +0800

    修正disable

--- js/normal.js
+++ js/normal.js
@@ -107,6 +107,11 @@ $(".webLinks.playHelp").on("click",function()
 {
     $('.coverClose')[0].click();
     let article=document.querySelector('.coverContent.article');
+    let next= document.querySelector(".nextPageButton");
+   
+    let last=document.querySelector(".lastPageButton");
+    if(next.classList.contains("disable"))next.classList.remove("disable");
+    if(!last.classList.contains("disable"))last.classList.add("disable");
     article.id="playHelp";
     article.dataset.page=0;
     $('.coverContent.article>.pages').load("pages/playHelp/0.html",function(){$(this).fadeIn(200);}).hide();
