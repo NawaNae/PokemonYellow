b787431b49574022825e4339a849e4e67c877d2f
NawaNawa <ltue2129@gmail.com>
2018-05-03 00:14:35 +0800

    新增 綁定物件函數 並於遊戲載入前載入

---
+++ start/js/autoBind.js
@@ -0,0 +1,9 @@
+function autoBind(obj) {
+    for (let o = obj; o; o = Object.getPrototypeOf(o)){
+        for (let name of Object.getOwnPropertyNames(o)){
+            if (typeof obj[name] === 'function'){
+                obj[name] = obj[name].bind(obj);
+            }
+        }
+    }
+}
--- start/js/load2.js
+++ start/js/load2.js
@@ -1,6 +1,7 @@
 //ref: http://blog.darkthread.net/blogs/darkthreadtw/archive/2009/01/15/4061.aspx
 Load.js.import(
     [
+        { src: define.frameworkPath + 'autoBind.js', lookFor: 'autoBind' },//載入綁定物件function 
         { src: define.frameworkPath + 'loadFramework.js', lookFor: 'loadFrameworkEnd' }//載入遊戲框架並載入遊戲
     ]
 );
