75c5e010b92a828559bf84c92e26980658c4de92
PrismRealm <alan159753@gmail.com>
2018-05-05 16:13:38 +0800

    新增 HPBar可透過百分比數控制

--- start/js/hpBarContainer.js
+++ start/js/hpBarContainer.js
@@ -6,6 +6,7 @@
  * @prop {HTMLDivElement} divHP 生命條的HTML元素。
  * @prop {number} maxHP 最大生命值。
  * @prop {number} HP 當前生命值。
+ * @prop {number} percent 生命值條的百分比數。
  */
 GameSystem.Classes.HPBarContainer =
 class HPBarContainer {
@@ -29,8 +30,8 @@ class HPBarContainer {
     get MaxHP() { return this._maxHP; }
 
     /**
-     * 設定血量。
-     * @prop {number} newHP 新的當前生命值。
+     * 設定血量。若兩參數皆不給，則刷新上一次所儲存的HP與maxHP。
+     * @prop {number?} newHP 新的當前生命值。
      * @prop {number?} newMaxHP 新的最大生命值。
      */
     updateHP(newHP, newMaxHP) {
@@ -40,22 +41,52 @@ class HPBarContainer {
         }
 
         // 設定HP
-        if (newHP > this._maxHP) 
-            this._HP = this._maxHP;
-        else if (newHP < 0)
-            this._HP = 0;
-        else
-            this._HP = newHP;
+        if (newHP)
+            if (newHP > this._maxHP) 
+                this._HP = this._maxHP;
+            else if (newHP < 0)
+                this._HP = 0;
+            else
+                this._HP = newHP;
 
         // 更新顯示
-        let percent = Math.ceil((this._HP / this._maxHP) * 100);
-        console.log(percent);
-        if (percent > 50)
-            this.divHP.style = 'width: ' + percent + '%; ' + 'background: lime;';
-        else if (25 < percent && percent <= 50)
-            this.divHP.style = 'width: ' + percent + '%; ' + 'background: yellow;';
+        this._percent = Math.ceil((this._HP / this._maxHP) * 100);
+        this._updateStyle();
+    }
+
+    /**
+     * 生命條增加1%。此動作不會影響到HP與maxHP的紀錄。
+     * 若百分比為0則不動作。
+     */
+    increaseOnePercent() {
+        if (this._percent > 0) {
+            this._percent -= 1;
+            this._updateStyle();
+        }
+    }
+
+    /**
+     * 生命條減少1%。此動作不會影響到HP與maxHP的紀錄。
+     * 若百分比為100則不動作。
+     */
+    decreaseOnePercent() {
+        if (this._percent > 0) {
+            this._percent -= 1;
+            this._updateStyle();
+        }
+    }
+
+    /**
+     * 更新生命條樣式。
+     * @private
+     */
+    _updateStyle() {
+        if (this._percent > 50)
+            this.divHP.style = 'width: ' + this._percent + '%; ' + 'background: lime;';
+        else if (25 < this._percent && this._percent <= 50)
+            this.divHP.style = 'width: ' + this._percent + '%; ' + 'background: yellow;';
         else
-            this.divHP.style = 'width: ' + percent + '%; ' + 'background: red;';
+            this.divHP.style = 'width: ' + this._percent + '%; ' + 'background: red;';
     }
 
     /**
