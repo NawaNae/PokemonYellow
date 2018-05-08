6c3d3e0ac342890e0ec6d69a55994ff44f42363b
NawaNawa <ltue2129@gmail.com>
2018-05-08 22:28:11 +0800

    實作 移動劇情內容PlotContent MoveCharacter

--- start/js/plotContents.moveCharacter.js
+++ start/js/plotContents.moveCharacter.js
@@ -1,21 +1,23 @@
 GameSystem.Classes.PlotContents.MoveCharacter=
 class MoveCharacter extends GameSystem.Classes.PlotContent
 {
-    constructor(options={name:"moveCharacter",type:"MoveCharacter",character:GameSystem.protagonist,from:undefined,to:undefined,vector:undefined})
+    constructor(options={name:"moveCharacter",type:"MoveCharacter",character:undefined,from:undefined,to:undefined,vector:undefined})
     {
         super(options);
+        
         this.character=options.character;
         this.from=options.from;
         this.to=options.to;
         this.vector=options.vector;
+        autoBind(this);
     }
     start()
     {
+        this.character=this.character||this.plot.npc;
         this.character.position=this.from||this.character.position;
-        
-    }
-    end()
-    {
-        this.plot.step();//下一步
+        if(!this.to&&!this.vector){console.log("cannot get to or vector from a move plot content.");return;}
+        this.to=this.to||this.from.add(this.vector);
+        this.character.moveTo(this.to,()=>{this.plot.step();});
     }
 }
+ 
