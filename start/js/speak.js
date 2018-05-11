e23fed63cbacea5ec875d39974a61f4175403b6b
NawaNawa <ltue2129@gmail.com>
2018-05-11 19:28:34 +0800

    測試 2

--- start/js/speak.js
+++ start/js/speak.js
@@ -1,8 +1,10 @@
 function speak(s,lang="zh-tw") {
     var protocol=location.protocol;
-    s=((protocol==="http:"?"http:":protocol==="file:"?"https:":protocol)+"//translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&q="+encodeURI(s)+"&tl="+lang);
+    
     if(location.hostname==="nawanae.github.io"&&protocol==="https:")
         s="https://myweb.ntut.edu.tw/~t105590029/php/googleTTSproxy.php?Text="+s+"&Lang="+lang;//proxy
+    else
+    s=((protocol==="http:"?"http:":protocol==="file:"?"https:":protocol)+"//translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&q="+encodeURI(s)+"&tl="+lang);
     var a=new Audio(s);
     a.autoplay=true;
     return a;
