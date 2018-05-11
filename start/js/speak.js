1cf606cc0cbbe4f5855b13221aca40b6d8ee61f5
NawaNawa <ltue2129@gmail.com>
2018-05-11 18:57:56 +0800

    修正 https speak

--- start/js/speak.js
+++ start/js/speak.js
@@ -1,6 +1,6 @@
 function speak(s,lang="zh-tw") {
     var protocol=location.protocol;
-    s=((protocol==="http:"?"http:":protocol==="https:"||protocol==="file:"?"https:":protocol)+"//translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q="+encodeURI(s)+"&tl="+lang);
+    s=((protocol==="http:"?"http:":protocol==="https:"||protocol==="file:"?"https:":protocol)+"//translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&q="+encodeURI(s)+"&tl="+lang);
     var a=new Audio(s);
     a.autoplay=true;
     return a;
