42351b1994bbf6728e53c180e97740f6a2ab2c44
NawaNawa <ltue2129@gmail.com>
2018-05-14 02:20:37 +0800

    簡短 Loaded初始化 使用存檔/新遊戲來輸入信息

--- start/js/gameSystemLoaded.js
+++ start/js/gameSystemLoaded.js
@@ -1,16 +1,12 @@
 GameSystem.loaded=function ()
 {
-    var GS=GameSystem;
-    var CS=GS.Classes;
-    GS.rival=new CS.Character("屁孩茂",CS.Character.Face.Down);
-    GS.protagonist=new CS.Protagonist(
-        "小智障", //name
-        new CS.Position(4,4) //position
-    );
+    var GS=GameSystem,CS=GS.Classes,Image=CS.Image;
+    var x;
+    GS.protagonist=new CS.Protagonist();
+    GS.rival=new CS.Character(x,x,x,new Image(define.characterImagePath+"rival.png"),10);
     let protagonist=GS.protagonist;
     protagonist.initialize();
     protagonist.image.addToAllLevels();
-    GameSystem.HTMLObjectContainer.options.find("角色資料").text="$MY_NAME";//強制更新清單內容 存檔之後放在載入後執行
 }
 
 GameSystem.loaded();
