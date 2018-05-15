ceec885dd61f11293862e884c2edeeb107a71cb9
NawaNawa <ltue2129@gmail.com>
2018-05-16 01:45:08 +0800

    新增 debug快速定義函數

---
+++ start/js/debug.js
@@ -0,0 +1,15 @@
+
+GameSystem.debugDefine=function()
+{
+    GS=GameSystem;
+    CS=GS.Classes;
+    mainChar=GS.protagonist;
+    level=()=> Framework.Game._currentLevel;
+    Point=CS.Point;
+    Position=CS.Position;
+    container=GS.HTMLContainer;
+    GM=GS.Manager;
+    RM=GM.Records;
+    KM=GM.Key;
+    GR=GS.Recources;
+}
