8b9bdf7185314d6964f8fc174049a81f4536e82f
NawaNawa <ltue2129@gmail.com>
2018-05-16 01:46:46 +0800

    簡化 GameImage (繼承GameObject) 補齊於Scene中offset繪圖計算

--- start/js/image.js
+++ start/js/image.js
@@ -7,7 +7,7 @@ var GameSystem=GameSystem || {};
  * @property _cutSize : {x: int ,y: int} 儲存切割大小
  * @property src : str 儲存圖片網址 作為下載 及 儲存名稱
  */
-GameSystem.Classes.Image=class GameImage
+GameSystem.Classes.Image=class GameImage extends GameSystem.Classes.GameObject
 {
     /**
      * 
@@ -19,16 +19,16 @@ GameSystem.Classes.Image=class GameImage
      * cutStartPosition : {x:int, y:int} 切割圖片左上角的位置，
      * cutSize : {x:int, y:int} 切割圖片的寬與高。
      */
-    constructor(src,option={position:undefined,displaySize:undefined,cutStartPosition:undefined,cutSize:undefined})
+    constructor(src,option={position:undefined,point:undefined,displaySize:undefined,cutStartPosition:undefined,cutSize:undefined})
     {
-        this.position=option.position||{x:0,y:0};
+        super({position:option.position,point:option.point});
         this.displaySize=option.displaySize?option.displaySize.constructor.name=="Position"?option.displaySize.toPoint():option.displaySize:undefined;
         this.cutStartPosition=option.cutStartPosition?option.cutStartPosition.constructor.name=="Position"?option.cutStartPosition.toPoint():option.cutStartPosition:undefined;
         this.cutSize=option.cutSize?option.cutSize.constructor.name=="Position"?option.cutSize.toPoint():option.cutSize:undefined;
         this._src=src;
         this.image=Load.image(src);
         this.visible=true;
-      //  Framework.Game._currentLevel._allGameElement.push(this);//視為遊戲物件
+     
     }
     addToLevelByLevelName(name)
     {
@@ -45,19 +45,6 @@ GameSystem.Classes.Image=class GameImage
         {
             ele.level._allGameElement.push(this);
         });
-    }
-    load()
-    {
-        if(this.image)
-            Framework.Game._currentLevel.rootScene.attach(this.image);
-    }
-    initialize()
-    {
-        
-    }
-    update()
-    {
-
     }
     copy(image)
     {
@@ -102,20 +89,22 @@ GameSystem.Classes.Image=class GameImage
         this.displaySize.x=this.image.width*value.x;
         this.displaySize.y=this.image.height*value.y;
    }
-   clearDirtyFlag()
-   {
-
-   }
     draw(context)
     {
         if(!this.visible)return ;
+        var x=this.position.x,y=this.position.y;
+        if(this.spriteParent)
+        {
+            x+=this.spriteParent.absolutePosition.x;
+            y+=this.spriteParent.absolutePosition.y;
+        }
         if(this.cutStartPosition&&this.cutSize)
             if(this.displaySize)
                 context.drawImage(
                     this.image,
                     this.cutStartPosition.x,this.cutStartPosition.y,
                     this.cutSize.x,this.cutSize.y,
-                    this.position.x,this.position.y,
+                    x,y,
                     this.scale.x*this.cutSize.x,this.scale.x*this.cutSize.y
                 );
             else
@@ -123,19 +112,19 @@ GameSystem.Classes.Image=class GameImage
                     this.image,
                     this.cutStartPosition.x,this.cutStartPosition.y,
                     this.cutSize.x,this.cutSize.y,
-                    this.position.x,this.position.y,
+                    x,y,
                     this.cutSize.x,this.cutSize.y
                 );
         else if(this.displaySize)
             context.drawImage(
                 this.image,
-                this.position.x,this.position.y,
+                x,y,
                 this.displaySize.x,this.displaySize.y
             );
         else
             context.drawImage(
                 this.image,
-                this.position.x,this.position.y,
+                x,y,
             );
     }
     
