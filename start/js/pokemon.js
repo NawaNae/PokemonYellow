3e1deb66531ea91327a2a2436c96edc1210826f1
PrismRealm <alan159753@gmail.com>
2018-05-17 20:14:41 +0800

    除錯 新增測試用的皮卡丘、小拉達寶可夢

--- start/js/pokemon.js
+++ start/js/pokemon.js
@@ -180,7 +180,6 @@ class Pokemon extends GameSystem.Classes.StandardStat {
 (() => {
     let DEX = GameSystem.Classes.PokemonType.Dictionary;
     GameSystem.Resource.Debug = GameSystem.Resource.Debug || {};
-    GameSystem.Resource.Debug.pokemon1 = new GameSystem.Classes.Pokemon("妙蛙種子", DEX["妙蛙種子"]);
-    GameSystem.Resource.Debug.pokemon2 = new GameSystem.Classes.Pokemon("小火龍", DEX["小火龍"]);
-    GameSystem.Resource.Debug.pokemon3 = new GameSystem.Classes.Pokemon("傑尼龜", DEX["傑尼龜"]);
+    GameSystem.Resource.Debug.pikachu = new GameSystem.Classes.Pokemon("皮卡丘", DEX["皮卡丘"]);
+    GameSystem.Resource.Debug.rattata = new GameSystem.Classes.Pokemon("小拉達", DEX["小拉達"]);
 })();
