e78ca18e25e54eb107eecd931766137212629915
NawaNawa <ltue2129@gmail.com>
2018-05-21 00:14:15 +0800

    新增 物品使用函數

--- start/js/propItem.js
+++ start/js/propItem.js
@@ -10,11 +10,19 @@ class PropItem {
     /**
      * @param {string} name 道具名稱。
      * @param {number} count 道具的堆疊數量。
+     * @param {function} use 使用呼叫的函數 若使用成功請回傳true
      */
-    constructor(name, count = 1) {
+    constructor(name, count = 1, use, isAutoDecreasing=true) {
         this._name = name;
         this._count = count;
+        this._use=()=>{
+            if(this.use)var result=this.use();
+            if(this.isDecreasing&&result)this.count--;
+        }
+        this.isDecreasing=isAutoDecreasing;
     }
+    set isDecreasing(bool){if(typeof this._count==="undefined"){console.warn("請確保有count的情況下再設定");return;}this._isDecreasing=bool;}
+    get isDecreasing(){return this._isDecreasing;}
 
     get name() { return this._name; }
     set name(newName) { this._name = newName; }
