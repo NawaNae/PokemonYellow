43085225b80e0296838d9f0818832373663234a6
PrismRealm <alan159753@gmail.com>
2018-05-25 18:22:29 +0800

    變更 戰鬥資訊變化能力階級之方法

--- start/js/battleInfo.js
+++ start/js/battleInfo.js
@@ -21,103 +21,112 @@ class BattleInfo {
         this._accuracyLevel = 0;
         this._evasionRateLevel = 0;
     }
-
+    
+    /** 寶可夢本身的資訊。 */
     get pokemon() { return this._pokemon; }
+
+    /** 寶可夢的名稱。 */
+    get name() { return this._pokemon.name; }
+
+    /** 寶可夢的最大血量。 */
+    get maxHP() { return this._pokemon.maxHP; }
+
+    /** 寶可夢的等級。 */
+    get level() { return this._pokemon.level; }
+
+    /** 寶可夢的第一屬性。 */
+    get typeA() { return this._pokemon.typeA; }
+
+    /** 寶可夢的第二屬性。 */
+    get typeB() { return this._pokemon.typeB; }
 
     // #region ======================================== 階級變換類 ========================================
 
     /**
-     * 變更攻擊階級。
-     * @param {number} diff 攻擊階級的差。
-     * @return {boolean} 是否成功更改了階級。
+     * 指定能力種類，對其進行階級變化。
+     * @param {GameSystem.Classes.Move} statusMove 變化的招式。
+     * @return {number} 變更後的階級。
      */
-    changeAttackLevel(diff) {
-        if (this._attackLevel == 6 && diff > 0 || this._attackLevel == -6 && diff < 0){
-            return false;
+    changeStatLevel(statusMove) {
+        const StatType = GameSystem.Classes.Move.StatType;
+        let statType = statusMove.statType;
+        switch(statType) {
+            case StatType.Attack:
+                return this._changeAttackLevel(statusMove);
+            case StatType.Defnese:
+                return this._changeDefenseLevel(diff);
+            case StatType.Speed:
+                return this._changeSpeedLevel(diff);
+            case StatType.Accuracy:
+                return this._changeAccuracyLevel(diff);
+            case StatType.EvasionRate:
+                return this._changeEvasionLevel(diff);;
         }
-        else {
-            this._attackLevel = this._levelFilter(this._attackLevel + diff);
-            return true;
-        }
+    }
+
+    /**
+     * 變更攻擊階級。
+     * @private
+     * @param {number} diff 攻擊階級的差。
+     * @return {number} 階級變化差。
+     */
+    _changeAttackLevel(diff) {
+        let originalLevel = this._attackLevel;
+        this._attackLevel = this._levelFilter(this._attackLevel + diff);
+        return this._attackLevel - originalLevel;
     }
 
     /**
      * 變更防禦階級。
+     * @private
      * @param {number} diff 防禦階級的差。
-     * @return {boolean} 是否成功更改了階級。
+     * @return {number} 階級變化差。
      */
-    changeDefenseLevel(diff) {
-        if (this._defenseLevel == 6 && diff > 0 || this._defenseLevel == -6 && diff < 0){
-            return false;
-        }
-        else {
-            this._defenseLevel = this._levelFilter(this._defenseLevel + diff);
-            return true;
-        }
-    }
-
-    /**
-     * 變更特殊階級。
-     * @param {number} diff 特殊階級的差。
-     * @return {boolean} 是否成功更改了階級。
-     */
-    changeSpecialLevel(diff) {
-        if (this._specialLevel == 6 && diff > 0 || this._specialLevel == -6 && diff < 0){
-            return false;
-        }
-        else {
-            this._specialLevel = this._levelFilter(this._specialLevel + diff);
-            return true;
-        }
+    _changeDefenseLevel(diff) {
+        let originalLevel = this._defenseLevel;
+        this._defenseLevel = this._levelFilter(this._defenseLevel + diff);
+        return this._defenseLevel - originalLevel;
     }
 
     /**
      * 變更速度階級。
+     * @private
      * @param {number} diff 速度階級的差。
-     * @return {boolean} 是否成功更改了階級。
+     * @return {number} 階級變化差。
      */
-    changeSpeedLevel(diff) {
-        if (this._speedLevel == 6 && diff > 0 || this._speedLevel == -6 && diff < 0){
-            return false;
-        }
-        else {
-            this._speedLevel = this._levelFilter(this._speedLevel + diff);
-            return true;
-        }
+    _changeSpeedLevel(diff) {
+        let originalLevel = this._speedLevel;
+        this._speedLevel = this._levelFilter(this._speedLevel + diff);
+        return this._speedLevel - originalLevel;
     }
 
     /**
      * 變更精精準階級。
+     * @private
      * @param {number} diff 精準階級的差。
-     * @return {boolean} 是否成功更改了階級。
+     * @return {number} 階級變化差。
      */
-    changeAccuracyLevel(diff) {
-        if (this._accuracyLevel == 6 && diff > 0 || this._accuracyLevel == -6 && diff < 0){
-            return false;
-        }
-        else {
-            this._accuracyLevel = this._levelFilter(this._accuracyLevel + diff);
-            return true;
-        }
+    _changeAccuracyLevel(diff) {
+        let originalLevel = this._accuracyLevel;
+        this._accuracyLevel = this._levelFilter(this._accuracyLevel + diff);
+        return this._accuracyLevel - originalLevel;
     }
 
     /**
      * 變更迴避階級。
+     * @private
      * @param {number} diff 迴避階級的差。
-     * @return {boolean} 是否成功更改了階級。
+     * @return {number} 階級變化差。
      */
-    changeEvasionLevel(diff) {
-        if (this._evasionRateLevel == 6 && diff > 0 || this._evasionRateLevel == -6 && diff < 0){
-            return false;
-        }
-        else {
-            this._evasionRateLevel = this._levelFilter(this._evasionRateLevel + diff);
-            return true;
-        }
+    _changeEvasionLevel(diff) {
+        let originalLevel = this._evasionRateLevel;
+        this._evasionRateLevel = this._levelFilter(this._evasionRateLevel + diff);
+        return this._evasionRateLevel - originalLevel;
     }
 
     /**
      * 等級過濾器。檢查等級是否有在[-6,6]之間的範圍，若有則修正之。
+     * @private
      * @param {number} level 修改過的等級。
      * @return {number} 修正後的等級。
      */
@@ -139,7 +148,7 @@ class BattleInfo {
      * @return {number} 在戰鬥台上的攻擊力。
      */
     get attack() {
-        return (2 + Math.max(0, this._attackLevel)) / (2 + Math.max(0, -this._attackLevel)) * this.pokemon.attack;
+        return (2 + Math.max(0, this._attackLevel)) / (2 + Math.max(0, -this._attackLevel)) * this._pokemon.attack;
     }
 
     /**
@@ -147,15 +156,7 @@ class BattleInfo {
      * @return {number} 在戰鬥台上的防禦力。
      */
     get defense() {
-        return (2 + Math.max(0, this._defenseLevel)) / (2 + Math.max(0, -this._defenseLevel)) * this.pokemon.defense;
-    }
-
-    /**
-     * 取得寶可夢在戰鬥台上的特殊值。
-     * @return {number} 在戰鬥台上的特殊值。
-     */
-    get special() {
-        return (2 + Math.max(0, this._specialLevel)) / (2 + Math.max(0, -this._specialLevel)) * this.pokemon.special;
+        return (2 + Math.max(0, this._defenseLevel)) / (2 + Math.max(0, -this._defenseLevel)) * this._pokemon.defense;
     }
 
     /**
@@ -208,7 +209,7 @@ class BattleInfo {
         this._pokemon = pokemon;
         this._attackLevel = 0;
         this._defenseLevel = 0;
-        this._specialLevel = 0;
+        this._speedLevel = 0;
         this._accuracyLevel = 0;
         this._evasionRateLevel = 0;
     }
