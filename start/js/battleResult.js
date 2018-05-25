e9d9af0c7f26f5c6718133523d5b35c1ad60f8a2
PrismRealm <alan159753@gmail.com>
2018-05-25 18:51:57 +0800

    變更 對手寶可夢昏厥後移出畫面方法

--- start/js/battleResult.js
+++ start/js/battleResult.js
@@ -118,9 +118,9 @@ class BattleResult {
         let delay = 100;                    // 延遲 100 ticks
         let battleLevel_animationSet;       // 紀錄 BattleLevel 上的 animationSet
         /** 移動對手寶可夢的函式 */
-        function MovingOut(ctx) {
+        function MovingOut(ctx, image) {
             if (opponentPos.x < 145) {      // 對手寶可夢移出畫面
-                ctx.drawImage(this._opponentPokemonImage, opponentPos.x, opponentPos.y);
+                ctx.drawImage(image, opponentPos.x, opponentPos.y);
                 opponentPos.x += 1;
             }
             else if (delay > 0) {           // 延遲
