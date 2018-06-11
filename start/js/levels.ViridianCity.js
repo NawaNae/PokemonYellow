49644ef694ed13f8102fb1e44f86d6f31910b494
NawaNawa <ltue2129@gmail.com>
2018-06-12 00:48:38 +0800

    完成 常盤市mart外觀

--- start/js/levels.ViridianCity.js
+++ start/js/levels.ViridianCity.js
@@ -8,6 +8,8 @@ class ViridianCity extends GameSystem.Classes.Level {
         var Event=CS.Event, Plot=CS.Plot, Position=CS.Position, MoveChar=PC.MoveCharacter;
         var e0=new Event(new Position(12,10),()=>{for(let i=0;i<this.npcs.length;i++)this.npcs[i].moveTo(GameSystem.protagonist.position)});
         this.events.push(e0);
+        this.music= Load.audio( define.musicPath+"background/106 The Road To Viridian City From Palette.mp3");
+        this.music.loop=true;
         var oak=new NPC("doctorOak",CS.Character.Face.Down,new Position(14, 9),new Image(define.characterImagePath + "oak.png", { cutStartPosition: new Position(1, 0), cutSize: new Position(1, 1) }),4, Drama.OakNormal);
     }
     initGates()
@@ -18,17 +20,18 @@ class ViridianCity extends GameSystem.Classes.Level {
         pushItem("route01",10,0,"viridianCity",20,31);
         pushItem("route01",11,0,"viridianCity",21,31);
         pushItem("viridianCityHospital",4,7,"viridianCity",23,25);
-          //  pushItem("mart",0,0,"viridianCity",29,19);
+        pushItem("viridianCityMart",4,7,"viridianCity",29,19);
     }
     initSubLevels()
     {
-        let hospital = new GameSystem.Classes.Levels.ViridianCity.Hospital();
-
-         this.subLevels.push(hospital);
-
+        let hospital = new GameSystem.Classes.Levels.ViridianCity.Hospital("viridianCityHospital");
+        let mart=new GameSystem.Classes.Levels.ViridianCity.Mart("viridianCityMart");
+        this.subLevels.push(hospital);
+        this.subLevels.push(mart);
          for(let level of this.subLevels)
              level.parentLevel=this;
          Framework.Game.addNewLevel({"viridianCityHospital":hospital});
+         Framework.Game.addNewLevel({"viridianCityMart":mart});
     }
     initObstacles()
     {
@@ -98,9 +101,7 @@ class ViridianCity extends GameSystem.Classes.Level {
     load() {
         super.load();
         var GS = GameSystem,CS = GS.Classes, KM = GS.Manager.Key,Position=CS.Position;
-        this.music= Load.audio( define.musicPath+"background/106 The Road To Viridian City From Palette.mp3");
-        this.music.loop=true;
-        this.music.autoplay=true;
+        this.music.play();
         this.size.pos1=new CS.Position(0,0);
         this.size.pos2=new CS.Position(35,30);
         this.map=new Framework.Scene();
@@ -134,58 +135,13 @@ class Hospital extends GameSystem.Classes.Levels.Hospital {
         pushItem("viridianCityHospital",4,7,"viridianCity",23,25);
     }
 }
-// class ProtagonistHome2F extends GameSystem.Classes.Level {
-//     constructor() {
-//         let CS = GameSystem.Classes;
-//         super(new CS.Position(7, 7), 'palletTownHome2F');
-//         this.size.pos1=new CS.Position(0, 1);
-//         this.isSubLevel = true;
-//     }
-
-//     initGates() {
-//         var CS = GameSystem.Classes;
-//         this.gates.push(new CS.Connection
-//             (new CS.MapPosition
-//                 (
-//                 "protagonistHome2F",
-//                 new CS.Position(7, 1)
-//                 ),
-//             new CS.MapPosition
-//                 (
-//                 "protagonistHome1F",
-//                 new CS.Position(7, 1)
-//                 )
-//             ));
-//     }
-//     initSignBoards() {
-//         var GS = GameSystem, CS = GS.Classes;
-//         var SignBoard = CS.SignBoard, Position = CS.Position, Polt = CS.Polt, Paragraph = CS.Paragraph, Drama = GS.Resource.Drama.PalletTown;
-//         this.signBoards.push(new SignBoard({ position: new Position(0, 7), content: Drama.PronBooks }));
-
-//     }
-//     initObstacles()
-//     {
-//         var GS = GameSystem;
-//         var CS = GS.Classes;
-//         this.obstacles.push(new CS.Rectangle({ x: 6, y: 6 }, { x: 6, y: 7 }));
-//         this.obstacles.push(new CS.Rectangle({ x: 3, y: 4 }, { x: 3, y: 5 }));
-//         this.obstacles.push(new CS.Rectangle({ x: 0, y: 1 }, { x: 2, y: 1 }));
-//     }
-//     load()
-//     {
-//         super.load();
-//         this.music= Load.audio( define.musicPath+"background/102 Palette Town Theme.mp3");
-//         this.music.loop=true;
-//         this.music.autoplay=true;
-//         var CS=GameSystem.Classes;
-//         var GS=GameSystem;
-//         this.mapImage=new CS.Image(define.imagePath+"palletTownHome2F.png");
-//         this.map=new Framework.Scene;
-//         this.map.x = GS.protagonist._screenPosition.toPoint().x - GS.protagonist.position.toPoint().x;
-//         this.map.y = GS.protagonist._screenPosition.toPoint().y - GS.protagonist.position.toPoint().y;
-//         this.map.attach(this.mapImage);
-//         this.walker=new CS.MapWalker({mapRef:this.map});
-//         this.rootScene.attach(this.map);
-//         this.rootScene.attach(GS.protagonist.image);
-//     }
-// }
+GameSystem.Classes.Levels.ViridianCity.Mart=
+class Mart extends GameSystem.Classes.Levels.Mart {
+    initGates() {
+        var GS = GameSystem,CS = GS.Classes;
+        var newConnectionItem=(map1,x1,y1,map2,x2,y2)=>new CS.Connection(new CS.MapPosition(map1,new CS.Position(x1,y1)),new CS.MapPosition(map2,new  CS.Position(x2, y2)));
+        var pushItem=(map1,x1,y1,map2,x2,y2)=>this.gates.push(newConnectionItem(map1,x1,y1,map2,x2,y2));
+        pushItem("viridianCityMart",3,7,"viridianCity",29,19);
+        pushItem("viridianCityMart",4,7,"viridianCity",29,19);
+    }
+}
