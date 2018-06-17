19c95d1e17436e35cb8638fec1948b6c0aaffad7
NawaNawa <ltue2129@gmail.com>
2018-06-17 14:52:35 +0800

    同步金錢

--- start/js/moneyDialog.js
+++ start/js/moneyDialog.js
@@ -5,21 +5,22 @@ class MoneyDialog extends DisplayInformation.Digit
     constructor()
     {
         super(0,"","円","moneyDialog","div");
-        this.displayClassName=['hide','show'];
         this.visible=false;
+    }
+    onShow()
+    {
+        this.updateMoney();
+        this.timeNumber=setInterval(()=>this.updateMoney(),250);
+    }
+    onHide()
+    {
+        if(this.timeNumber)
+            clearInterval(this.timeNumber);
     }
     updateMoney()
     {
+        console.log("update")
         if(GameSystem&&GameSystem.protagonist)
         this.value=GameSystem.protagonist.money;
-    }
-    get visible()
-    {return !this._display.classList.contains(this.displayClassName[0]);}
-    set visible(value)
-    {
-        if(value)
-        this.updateMoney();
-        this._display.classList.add(this.displayClassName[value|0]);
-        this._display.classList.remove(this.displayClassName[(!value)|0]);
     }
 }
